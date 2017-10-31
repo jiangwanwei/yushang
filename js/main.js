b38c66a86461caca37fdd02a536b9f1ede0612f4
姜万伟 <jiangwanwei@jiangwanweideMacBook-Pro.local>
2017-10-31 18:14:05 +0800

    图片加载状态判断

--- js/main.js
+++ js/main.js
@@ -1,30 +1,60 @@
 $(document).ready(function() {
 	// loading 效果
-	setTimeout(function(){
-		$('#loading-txt').fadeIn('slow', function() {
-			var loading = $('#loading');
-			loading.delay(1300).fadeOut('500', function() {
-				$(this).remove();
+	imgLoad(['../images/page2-logo.png', '../images/page1-1.png', '../images/page1-2.png'])
+	.done(function(){
+		setTimeout(function(){
+			$('#loading-txt').fadeIn('slow', function() {
+				var loading = $('#loading');
+				loading.delay(1300).fadeOut('500', function() {
+					$(this).remove();
 
-				var slide = $('.swiper-slide').length - 1;
+					var slide = $('.swiper-slide').length - 1;
 
-				// swiper
-				var mySwiper = new Swiper ('.swiper-container', {
-				    direction: 'horizontal',
-				    // loop: true,
+					// swiper
+					var mySwiper = new Swiper ('.swiper-container', {
+					    direction: 'horizontal',
+					    // loop: true,
 
-				    autoplay: 6000,
+					    autoplay: 6000,
 
-				    speed: 300,
-				    
-				    // // 如果需要前进后退按钮
-				    // nextButton: '.swiper-button-next1',
-				    // prevButton: '.swiper-button-prev1',
-				    onTouchEnd: function(swiper){
-				    	if (swiper.activeIndex === slide) location = './form.html'
-				    }
-				}) 
+					    speed: 300,
+					    
+					    // // 如果需要前进后退按钮
+					    // nextButton: '.swiper-button-next1',
+					    // prevButton: '.swiper-button-prev1',
+					    onTouchEnd: function(swiper){
+					    	if (swiper.activeIndex === slide) location = './form.html'
+					    }
+					}) 
+				});
 			});
-		});
-	}, 500)	
+		}, 100)	
+	})	
 });
+
+// 图片加载
+function imgLoad(imgs) {
+	if (!imgs.length) return;
+
+	var count = 0,
+		callfunc = function() {},
+	loadHandle = function () {
+		count += 1;
+		if (imgs.length === count) {
+			callfunc && callfunc(imgs)
+		}
+	}
+
+	imgs.forEach(function (img){
+		var imgtag = document.createElement('img');
+		imgtag.src = img;
+		imgtag.onload = loadHandle;
+		imgtag.onerror = loadHandle;
+	})
+	
+	return {
+		done: function(callback) {
+			callfunc = callback || callfunc;
+		}
+	}
+}
